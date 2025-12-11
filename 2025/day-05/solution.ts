f64638966fb8289ce68fa3d947477a8fd5d56481
moritz-grimm <molumo@icloud.com>
2025-12-11 22:04:54 +0100

    solve day-05/part-02

--- 2025/day-05/solution.ts
+++ 2025/day-05/solution.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "node:fs";
 
 function solvePart1(input: string): number {
     const start = performance.now();
-    const [ranges, ingredients] = parseInput(input);
+    const [ranges, ingredients] = parsePart1Input(input);
     const freshIngredients: number[] = [];
 
     for (const num of ingredients) {
@@ -11,7 +11,6 @@ function solvePart1(input: string): number {
                 freshIngredients.push(num);
             }
         }
-
     }
 
     const end = performance.now();
@@ -20,7 +19,39 @@ function solvePart1(input: string): number {
     return freshIngredients.length;
 }
 
-function parseInput(input: string): [{ start: number; end: number; }[], number[]] {
+function solvePart2(input: string): number {
+    const start = performance.now();
+    const numRanges = parsePart2Input(input);
+    const merged: { start: number, end: number }[] = [];
+    let ingredientCount = 0;
+
+    numRanges.sort((a, b) => a.start - b.start);
+
+    for (const range of numRanges) {
+        if (merged.length === 0) {
+            merged.push(range);
+        } else {
+            const last = merged[merged.length - 1];
+
+            if (range.start < last.end + 1) {
+                last.end = Math.max(range.end, last.end);
+            } else {
+                merged.push(range);
+            }
+        }
+    }
+
+    merged.forEach(element => {
+        ingredientCount += element.end - element.start + 1;
+    });
+
+    const end = performance.now();
+    console.log(`Runtime Part 2: ${end - start} ms`);
+
+    return ingredientCount;
+}
+
+function parsePart1Input(input: string): [{ start: number; end: number; }[], number[]] {
     const rawBlocks = input
         .trim()
         .split(/\n\s*\n/)
@@ -37,5 +68,23 @@ function parseInput(input: string): [{ start: number; end: number; }[], number[]
     return [ranges, ingredients];
 }
 
+function parsePart2Input(input: string): { start: number; end: number; }[] {
+    const rawBlocks = input
+        .trim()
+        .split(/\n\s*\n/)
+        .map(block => block.split(/\r?\n/));
+
+    return rawBlocks[0].map(line => {
+        const [start, end] = line.split("-");
+
+        return {
+            start: Number(start),
+            end: Number(end),
+        };
+    });
+}
+
 const input = readFileSync("input.txt", "utf-8");
 console.log("Result Part 1: " + solvePart1(input));
+console.log();
+console.log("Result Part 2: " + solvePart2(input));
